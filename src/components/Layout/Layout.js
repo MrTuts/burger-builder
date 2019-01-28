f9b125ba5d47914b48487158fc4fc3160f97838b
Pavel Gric <pavel@ideatech.cz>
2019-01-28 16:02:08 +0100

    [WIP] Add Navigation items

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
 
 const layout = props => (
   <>
-    <div>Toolbar, SideDrawer, Backdrop</div>
+    <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </>
 );

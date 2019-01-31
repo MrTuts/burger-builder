18970659b77e06253f2cfc34b1296b21c1e83876
Pavel Gric <pavel@ideatech.cz>
2019-01-31 13:21:04 +0100

    [Sample] move redux state update case into its own function

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -14,21 +14,22 @@ const initialState = {
   error: false,
 };
 
+const addIngredient = (state, action) => {
+  const updatedIngredient = updateObject({
+    [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+  });
+  const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+  const updatedState = {
+    ingredients: updatedIngredients,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
+  };
+  return updateObject(state, updatedState);
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      const updatedIngredient = updateObject({
-        [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
-      });
-      const updatedIngredients = updateObject(
-        state.ingredients,
-        updatedIngredient,
-      );
-      const updatedState = {
-        ingredients: updatedIngredients,
-        totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
-      };
-      return updateObject(state, updatedState);
+      return addIngredient(state, action);
     case actionTypes.REMOVE_INGREDIENT:
       return {
         ...state,
